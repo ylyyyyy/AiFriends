54e32207d12dd2d1c0ab4c2f76ac4e1379a743b8
ylyyyyy <972917671@qq.com>
2026-09-16 17:12:02 +0800

    实现前端编辑资料页面

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/user/account/logout',LogoutView.as_view()),
     path('api/user/account/register',RegisterView.as_view()),
     path('api/user/account/refresh_token',RefreshTokenView.as_view()),
-    path('api/user/account/get_user_info/',GetUserInfoView.as_view()),
+    path('api/user/account/get_user_info',GetUserInfoView.as_view()),
     path('api/user/profile/updata',UpdataProfileView.as_view()),
     path('',index),
 
--- frontend/src/views/user/profile/ProfileIndex.vue
+++ frontend/src/views/user/profile/ProfileIndex.vue
@@ -1,9 +1,71 @@
 <script setup>
+import Profile from "@/views/user/profile/components/Profile.vue";
+import Username from "@/views/user/profile/components/Username.vue";
+import Photo from "@/views/user/profile/components/Photo.vue";
+import {useUserStore} from "@/stores/user.js";
+import {ref, registerRuntimeCompiler, useTemplateRef} from "vue";
+import {base64ToFile} from "@/js/utils/base64_to_file.js";
+import api from "@/js/http/api.js";
 
+const user = useUserStore()
+
+const photoRef = useTemplateRef('photo-ref')
+const usernameRef = useTemplateRef('username-ref')
+const profileRef = useTemplateRef('profile-ref')
+const errorMessage = ref()
+
+async function handleUpdata(){
+  const photo = photoRef.value.myPhoto
+  const username = usernameRef.value.myUsername.trim()
+  const profile = profileRef.value.myProfile.trim()
+
+  errorMessage.value = ''
+  if (!photo) {
+    errorMessage.value='头像不能为空'
+  }else if (!username){
+    errorMessage.value = '用户名不能为空'
+  }else if (!profile){
+    errorMessage.value = '简介不能为空'
+  } else {
+    const formData = new FormData()
+    formData.append('username',username)
+    formData.append('profile',profile)
+    if(photo !== user.photo) {
+      formData.append('photo',base64ToFile(photo,'photo.png'))
+    }
+    try {
+      const res = await api.post('/api/user/profile/updata',formData)
+      const data = res.data
+      if (data.result === "success" ){
+        user.setUserInfo(data)
+      }else {
+        errorMessage.value = data.result
+      }
+    } catch (err){
+      console.log(err)
+    }
+  }
+
+}
 </script>
 
 <template>
-  编辑资料
+  <div class="ml-[33.33%]">
+    <div class="card w-120 bg-base-200 shadow-sm mt-30">
+      <div class="card-body">
+        <h3 class="text-lg font-bold my-4">编辑资料</h3>
+        <Photo ref="photo-ref" :photo="user.photo" />
+        <Username ref="username-ref" :username="user.username"/>
+        <Profile ref="profile-ref" :profile="user.profile"/>
+
+        <p v-if="errorMessage" class="text-sm text-red-500">{{errorMessage}}</p>
+
+        <div class="flex justify-center">
+          <button @click="handleUpdata" class="btn btn-neutral w-60 mt-2">更新</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
