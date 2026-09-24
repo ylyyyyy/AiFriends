98a70284a45c077992a9b5a4dd99a3c27ac364d1
ylyyyyy <972917671@qq.com>
2026-09-24 19:33:44 +0800

    实现搜索功能

--- backend/web/views/homepage/index.py
+++ backend/web/views/homepage/index.py
@@ -1,3 +1,5 @@
+
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -8,7 +10,14 @@ class HomepageIndexView(APIView):
     def get(self,request):
         try:
             items_count = int (request.query_params.get('items_count',0))
-            character_raw = Character.objects.all().order_by('-id')[items_count:items_count+20]
+            search_query = request.query_params.get('search_query','').strip()
+            if search_query:
+                queryset = Character.objects.filter(
+                    Q(name__icontains=search_query) | Q(profile__icontains=search_query)
+                )
+            else:
+                queryset = Character.objects.all()
+            character_raw = queryset.order_by('-id')[items_count:items_count+20]
             characters = []
             for character in character_raw:
                 author = character.author
--- frontend/src/views/homepage/HomepageIndex.vue
+++ frontend/src/views/homepage/HomepageIndex.vue
@@ -1,14 +1,16 @@
 <script setup>
 
-import {nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef} from "vue";
+import {nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef, watch} from "vue";
 import api from "@/js/http/api.js";
 import Character from "@/components/character/Character.vue";
+import {useRoute} from "vue-router";
+import router from "@/router/index.js";
 
 const characters = ref([])
 const isLoading = ref(false)
 const hasCharacters = ref(true)
 const sentinelRef = useTemplateRef('sentinel-ref')
-
+const route = useRoute()
 function checkSentinelVisible() {  // 判断哨兵是否能被看到
   if (!sentinelRef.value) return false
 
@@ -25,6 +27,7 @@ async function loadMore(){
     const res = await api.get('api/homepage/index',{
       params:{
         items_count:characters.value.length,
+        search_query:route.query.q || '',
       }
     })
     const data = res.data
@@ -63,6 +66,17 @@ onMounted( async () => {
       {root:null , rootMargin:'2px',threshold:0}
   )
   observer.observe(sentinelRef.value)
+})
+
+function reset(){
+  characters.value=[]
+  isLoading.value=false
+  hasCharacters.value =true
+  loadMore()
+}
+
+watch( () => route.query.q,newQ =>{
+  reset()
 })
 
 onBeforeUnmount( () =>{
